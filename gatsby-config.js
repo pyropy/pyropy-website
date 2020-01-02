62dcf4f3d65eaec75888230e6a82e1d47819952a
Srdjan S <stankovic.srdjo@gmail.com>
2020-01-02 17:48:52 +0100

    Update config with Github Graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,29 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [`gatsby-plugin-postcss`]
+  plugins: [
+    {
+      resolve: `gatsby-plugin-postcss`,
+      options: {}
+    },
+    {
+      resolve: `gatsby-source-graphql`,
+      options: {
+        fieldName: `github`,
+        typeName: `GitHub`,
+        createLink: () =>
+          createHttpLink({
+            uri: `https://api.github.com/graphql`,
+            headers: {
+              Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
+            },
+            fetch,
+          }),
+        createSchema: async () => {
+          const json = JSON.parse(fs.readFileSync(`${__dirname}/github.json`))
+          return buildClientSchema(json.data)
+        },
+      },
+    },
+  ],
 }
