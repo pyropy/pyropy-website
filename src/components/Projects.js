5c3a4b90162f35eb403d83aa3e7e3938433c6c82
Srdjan S <stankovic.srdjo@gmail.com>
2020-01-02 15:18:31 +0100

    Rename Card to ProjectCard

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,19 +1,19 @@
 import React, { useEffect, useState } from "react"
-import Card from "./Card"
+import ProjectCard from "./ProjectCard"
 import content from "../content.json"
 
 const Projects = () => {
   const [projects, setProjects] = useState([])
 
   useEffect(() => {
-    setProjects(content.content.projects)
+    setProjects(content.projects)
   }, [])
 
   return (
     <div className="container mx-auto px-5">
       <div className="inline-flex mb-4">
         {projects.map(project => (
-          <Card key={project.id} project={project} />
+          <ProjectCard key={project.id} project={project} />
         ))}
       </div>
     </div>
